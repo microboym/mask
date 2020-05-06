c89110024769fdc5017176ec7f24d17ed6ebac72
Tony Mai <microboym@outlook.com>
2020-05-06 19:27:20 +0800

    show all users in index

--- actions/home.go
+++ actions/home.go
@@ -1,9 +1,24 @@
 package actions
 
-import "github.com/gobuffalo/buffalo"
+import (
+	"mask/models"
+
+	"github.com/gobuffalo/buffalo"
+)
 
 // HomeHandler is a default handler to serve up
 // a home page.
 func HomeHandler(c buffalo.Context) error {
+	users, err := loadUsers()
+	if err != nil {
+		return err
+	}
+	c.Set("users", users)
 	return c.Render(200, r.HTML("index.html"))
+}
+
+func loadUsers() (models.Users, error) {
+	users := models.Users{}
+	err := models.DB.All(&users)
+	return users, err
 }
