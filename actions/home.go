af66ba874fe6176ffc65c2fcbdd0a6dc794e59b7
Tony Mai <microboym@outlook.com>
2020-05-08 09:58:40 +0800

    update home page
    create user-readble info
    links are not working now

--- actions/home.go
+++ actions/home.go
@@ -9,16 +9,21 @@ import (
 // HomeHandler is a default handler to serve up
 // a home page.
 func HomeHandler(c buffalo.Context) error {
-	users, err := loadUsers()
+	orderCount, err := models.DB.Count(models.Orders{})
 	if err != nil {
 		return err
 	}
-	c.Set("users", users)
+	captchaCount, err := models.DB.Where("answer = null").Count(models.Captchas{})
+	if err != nil {
+		return err
+	}
+	c.Set("orderCount", orderCount)
+	c.Set("captchaCount", captchaCount)
 	return c.Render(200, r.HTML("index.html"))
 }
 
-func loadUsers() (models.Users, error) {
-	users := models.Users{}
-	err := models.DB.All(&users)
-	return users, err
-}
+// func loadUsers() (models.Users, error) {
+// 	users := models.Users{}
+// 	err := models.DB.All(&users)
+// 	return users, err
+// }
