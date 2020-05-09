ec4458d8ffb9b8dd05533e845446808dc6ed0183
Tony Mai <microboym@outlook.com>
2020-05-09 17:37:10 +0800

    DB: remove column updated_at

--- migrations/20200429103021_create_orders.postgres.up.sql
+++ migrations/20200429103021_create_orders.postgres.up.sql
@@ -2,6 +2,5 @@ CREATE TABLE "orders" (
     "phone" char(11) references users("phone") NOT NULL check("phone" ~ '^[0-9]+$'),
     PRIMARY KEY("phone"),
     "img" bytea NOT NULL,
-    "created_at" timestamp NOT NULL default CURRENT_TIMESTAMP,
-    "updated_at" timestamp NOT NULL default CURRENT_TIMESTAMP
+    "created_at" timestamp NOT NULL default CURRENT_TIMESTAMP
 );
