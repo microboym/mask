53384038e78bd82cc590fba39824e489517d5ba9
Tony Mai <microboym@outlook.com>
2020-05-15 14:34:28 +0800

    Rename: image to bytes

--- models/bytes.go
+++ models/bytes.go
@@ -3,9 +3,17 @@ package models
 import "encoding/base64"
 
 // Image represents an image
-type Image interface {
+type Bytes interface {
 	// String encodes the image as base64
 	String() string
+}
+
+// SHA256 represents an some sha 256 data
+type SHA256 []byte
+
+// String encodes the sha 256 data as base64
+func (sha SHA256) String() string {
+	return "data:image/png;base64," + base64.StdEncoding.EncodeToString(sha)
 }
 
 // JPGImage represents an jpeg image
